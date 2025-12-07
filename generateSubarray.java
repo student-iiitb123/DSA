f2067be80591f107ae9c65c2ca7088c43752708b
Ashish singh Goutam <ashishgautam7696@gmail.com>
2025-12-07 15:30:02 +0530

    longest subarray

---
+++ generateSubarray.java
@@ -0,0 +1,16 @@
+
+
+public class generateSubarray {
+    public static void main(String[] args) {
+        int arr[] = {2,5,1,7,10};
+        for(int i=0;i<arr.length;i++){
+            for(int j =i;j< arr.length;j++){
+               for(int k =i;k<=j;k++){
+                System.out.print(arr[k] );
+               
+               }
+                System.out.println("");
+            }
+        }
+    }
+}
