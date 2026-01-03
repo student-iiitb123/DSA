ce0a42328a4a214c9139c1af139c54efe0c57d8f
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-01-03 18:56:39 +0530

    row wise binary search

---
+++ 2dArray.java/row.java
@@ -0,0 +1,26 @@
+
+
+public class row {
+    public static void main(String[] args) {
+         int[][] arr = {
+            {10,20,30,40},
+            {15,25,35,45},
+            {27,29,37,48},
+            {32,33,39,50}
+        };
+
+        int key = 29;
+        int sr =0;
+        int row = arr.length -1;
+        
+         while(sr > row) { 
+                
+         }
+
+        
+    
+
+
+    }
+    
+}
