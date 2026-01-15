d1e52ee30cfbf88220cf7c1f7f4e8ffd60b79b3b
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-01-15 17:00:25 +0530

    add Inversion

---
+++ DivideandConqueror/Inversion.java
@@ -0,0 +1,17 @@
+
+public class Inversion {
+    public static void main(String[] args) {
+        int arr[] = {4,5,1,3};
+        for(int i =0;i<arr.length;i++){
+            for(int j = i + 1;j<arr.length;j++){
+                if(i<j){
+                    if(arr[i] > arr[j]){
+                        System.out.println("(" + arr[i] + " " +  arr[j] + ")" );
+                    }
+                }
+
+            }
+        }
+
+    }
+}
