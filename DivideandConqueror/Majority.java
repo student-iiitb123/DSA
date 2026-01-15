ee8fe33e667628b148c2101562ed86c8c9bd25ca
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-01-15 12:28:46 +0530

    Optimize Majority element in array

--- DivideandConqueror/Majority.java
+++ DivideandConqueror/Majority.java
@@ -1,12 +1,38 @@
 
 
 public class Majority {
-    public static void majority(int arr[],int lo,int hi){
+public static int merge(int arr[], int lo, int mid, int hi,
+                        int leftMajor, int rightMajor) {
 
+    if (leftMajor == rightMajor) {
+        return leftMajor;
+    }
+
+    int count1 = 0;
+    int count2 = 0;
+
+    for (int i = lo; i <= hi; i++) {
+        if (arr[i] == leftMajor) count1++;
+        if (arr[i] == rightMajor) count2++;
+    }
+
+    return (count1 > count2) ? leftMajor : rightMajor;
+}
+
+    public static int majority(int arr[],int lo,int hi){
+        if(lo == hi) return arr[lo];
+        int mid = (lo+hi)/2;
+       int leftMajor = majority(arr, lo, mid);
+       int rightMajor = majority(arr, mid + 1, hi);
+
+
+        //combine time
+      return merge(arr, lo, mid, hi, leftMajor, rightMajor);
     }
     
     public static void main(String[] args) {
         int arr[] = {2,2,1,1,1,2,2};
-        majority(arr,0,arr.length-1);
+    int ans = majority(arr, 0, arr.length - 1);
+    System.out.println(ans);
     }
 }
