15603b4e9ae6980d848348cc3797135da299e7e4
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-01-17 17:02:00 +0530

    add combination sum

--- Backtracking/Comb.java
+++ Backtracking/Comb.java
@@ -1,18 +1,32 @@
 import java.util.ArrayList;
 
 public class Comb {
-    public static void comb(int arr[],int start,ArrayList<Integer> a){
+    public static void comb(int arr[],int start,ArrayList<Integer> a,int target,int sum){
          
 
-        if(start > arr.length){
+        if(sum > target){
+           
             return;       
          }
-         System.out.println(a);
+
+           if(sum == target){
+            System.out.println(a);
+            return;
+        }
+
+        
+         
+     
+      
+
         
          for(int i = start;i<arr.length;i++){
+         
             a.add(arr[i]);
-            comb(arr,i+1, a);
+          comb(arr, i, a, target, sum + arr[i]); 
             a.remove(a.size()-1);
+         
+          
 
          }
     
@@ -21,8 +35,9 @@ public class Comb {
     public static void main(String[] args){
         int arr[] = {1,2,3};
         //Declaring 
+        int target = 4;
         ArrayList<Integer> a = new ArrayList<>();
-      comb(arr,0,a);
+      comb(arr,0,a,target,0);
 
     }
 
