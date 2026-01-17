d26e3f5fa9191c8f4ea0f3274141bc539c04ce07
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-01-17 19:58:39 +0530

    Add phone number

---
+++ Backtracking/BruteForce.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class BruteForce {
+
+    public static void combination(String n, String arr[], ArrayList<String> result, int index, StringBuilder current) {
+
+       // Base case: if we've processed all digits
+        if (index == n.length()) {
+            result.add(current.toString());
+            return;
+
+        }
+
+
+
+        int number = n.charAt(index) - '0';
+        String letters = arr[number];
+
+        for (int j = 0; j < letters.length(); j++) {
+                                                   
+            // Add letter to current combination
+            current.append(letters.charAt(j));
+            // Recurse for next digit
+            combination(n, arr, result, index + 1, current); 
+            // Backtrack: remove last letter to try next option
+            current.deleteCharAt(current.length() - 1);
+
+
+        }
+    }
+
+         public static void main(String[] args) {
+         String n = "234";
+         String arr[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+         ArrayList<String> result = new ArrayList<>();
+         combination(n, arr, result, 0, new StringBuilder());
+         System.out.println(result);
+
+
+    }
+}
+
+
---
+++ Backtracking/PhoneNumber.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+
+public class PhoneNumber {
+
+    public static void  Combination(String n,String a[],ArrayList<String> s) {
+    
+
+        for(int i = 0;i<n.length();i++){
+            String sb = a[i];
+            for(int j =0;j<sb.length();j++){
+                
+                    
+            }
+
+
+        }
+
+   }
+    public static void main(String[] args) {
+        String n = "23";
+        String arr[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        Combination(n,arr,new ArrayList<>());
+
+
+    }
+}
