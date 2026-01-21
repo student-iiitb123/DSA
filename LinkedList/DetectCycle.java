f934aa4f1d418bbbd247b607c958c969b18080ed
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-01-21 17:25:54 +0530

    Floyd Cycle

---
+++ LinkedList/DetectCycle.java
@@ -0,0 +1,34 @@
+public class DetectCycle {
+      static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data= data;
+            this.next = null;
+        }
+    }
+
+    public static boolean Cycle(Node Head){
+        Node fast = Head;
+        Node slow = Head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+         Node Head = new Node(1);
+         Head.next = new Node(2);
+         Head.next.next = new Node(3);
+         Head.next.next.next = new Node(4);
+
+        boolean t = Cycle(Head);
+        System.out.print(t);
+         
+    }
+}
