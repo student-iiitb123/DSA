f07473f3724cd51035630f69b4f8fce852190a1d
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-02-10 15:03:23 +0530

    good String

---
+++ Stack/Removeouter.java
@@ -0,0 +1,25 @@
+public class Removeouter {
+class Solution {
+    public String removeOuterParentheses(String s) {
+
+        StringBuilder ans = new StringBuilder();
+        int depth = 0;
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == '(') {
+                if(depth > 0) ans.append(ch);
+                depth++;
+            }
+            else {
+                depth--;
+                if(depth > 0) ans.append(ch);
+            }
+        }
+
+        return ans.toString();
+    }
+}
+
+    
+}
