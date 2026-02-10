0818594bf7c112a83ba59fb2886116d06c3b4180
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-02-10 10:02:13 +0530

    add valid paranthesis

---
+++ Stack/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public 
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty())  return false;
+                int top = st.peek();
+                if(ch == ')' && top == '(' || ch == ']' && top == '[' || ch == '}' && top == '{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+
+
+
+        
+
+      
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
+ 
+    
+
