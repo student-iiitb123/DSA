c609d9e36d1408a6f097c4b20b24c9032f244763
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-02-10 15:01:39 +0530

    remove outer

---
+++ Stack/Count.java
@@ -0,0 +1,28 @@
+public record Count() {
+    public static void main(String[] args){
+      class Solution {
+    public String removeOuterParentheses(String s) {
+
+        StringBuilder ans = new StringBuilder();
+        int depth = 0;
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == '(') {
+                if(depth > 0) ans.append(ch);
+                depth++;
+            }
+            else {
+                depth--;
+                if(depth > 0) ans.append(ch);
+            }
+        }
+
+        return ans.toString();
+    }
+}
+
+
+    
+    }
+}
