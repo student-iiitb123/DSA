2f341f68470b3abf185e0372d826f9e18acc543c
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-02-19 12:19:11 +0530

    Circular quene

---
+++ CircularQueue.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class CircularQueue {
+
+    int arr[];
+    int front = -1;
+    int rear = -1;
+    int size;
+
+    CircularQueue(int size){
+        this.size = size;
+        arr = new int[size];
+    }
+
+    // ENQUEUE
+    void enqueue(int val){
+
+        // full
+        if((rear + 1) % size == front){
+            System.out.println("Queue Overflow");
+            return;
+        }
+
+        // first element
+        if(front == -1){
+            front = rear = 0;
+        }
+        else{
+            rear = (rear + 1) % size;
+        }
+
+        arr[rear] = val;
+    }
+
+    // DEQUEUE
+    void dequeue(){
+
+        if(front == -1){
+            System.out.println("Queue Underflow");
+            return;
+        }
+
+        System.out.println("Removed: " + arr[front]);
+
+        // last element
+        if(front == rear){
+            front = rear = -1;
+        }
+        else{
+            front = (front + 1) % size;
+        }
+    }
+
+    // PRINT
+    void display(){
+
+        if(front == -1){
+            System.out.println("Empty");
+            return;
+        }
+
+        int i = front;
+
+        while(true){
+            System.out.print(arr[i] + " ");
+
+            if(i == rear) break;
+
+            i = (i + 1) % size;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        CircularQueue q = new CircularQueue(5);
+
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+
+        q.dequeue();
+        q.dequeue();
+
+        q.enqueue(5);
+        q.enqueue(6);
+
+        q.display();
+    }
+}
