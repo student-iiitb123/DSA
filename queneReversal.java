98b16406760794b1a93a3043a0aa36cd79cf82d4
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-03-16 09:55:55 +0530

    quene reversal

---
+++ queneReversal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class queneReversal{
+    public static void main(String[] args) {
+        Quene<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        //loop kaha tak jayega ye nahi pata 
+    }
+}
