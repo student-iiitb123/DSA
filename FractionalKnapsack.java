f45d10238a39e6e9c51302b7b8c74ea525e3f62a
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-03-19 21:56:01 +0530

    Knapscak optimised

--- FractionalKnapsack.java
+++ FractionalKnapsack.java
@@ -1,23 +1,39 @@
+import java.util.*;
+
 public class FractionalKnapsack {
     public static void main(String[] args) {
         int weight[]= {10,20,30};
         int value[] = {60,100,120};
-        int maxCapacity = 50;
-        int sum = 0;
+      int W = 50;
+      double arr[][] = new double[weight.length][2];
+      for(int i =0;i<weight.length;i++){
+        arr[i][0] = 1;
+        arr[i][1] = value[i]/(double)weight[i];
+      }
+      Arrays.sort(arr,Comparator.comparingDouble(o -> o[1]));
+      int maxCapacity = W;
+      double sum =0;
 
-
-        for(int i =0;i<weight.length;i++){
-            int remainingweight = maxCapacity - weight[i];
-            if(remainingweight >= weight[i]){
-               sum = sum + value[i];
-            }
-            else{
-                int fractional = (value[i]/weight[i]) *remainingweight;
-                sum = sum + fractional;
-            }
-
-
+      for(int i =arr.length-1;i>=0;i--){
+        int idx =(int)arr[i][0];
+        if(weight[idx] <= maxCapacity){
+            sum = sum + value[idx];
+            maxCapacity = maxCapacity-weight[idx];
+           
         }
-        System.out.println(sum);
+        else{
+            double remain = arr[i][1] * maxCapacity;
+            sum = sum + remain;
+           break;
+        }
+      }
+
+      System.out.println(sum);
+
+
+
+
+
+
     }
 }
