936304ed1e6d083ab2c265c3b28832338edb3966
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-03-21 18:59:04 +0530

    dBMB problem

---
+++ Trees/tempCodeRunnerFile.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class height {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t >= 0){
+         int n = sc.nextInt();
+         int s = sc.nextInt();
+         int x = sc.nextInt();
+         int arr[] = new int[n];
+
+         for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+         }
+         int sum = 0;
+         for(int i =0;i<n;i++){
+            sum = sum + arr[i];
+         }
+         if(sum == s){
+            System.out.print("yes");
+         }
+         else if(sum < s){
+            while(sum < s){
+                sum = sum + x;
+                 }
+
+    if(sum == s){
+        System.out.println("Yes");
+    }
+    else{
+        System.out.println("No");
+    }
+
+            
+         }
+        
+        }
+    }
+    
+}
