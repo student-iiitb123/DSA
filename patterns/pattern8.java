895291f34e8d862dc78d4fd5d6b5f7d5f381255d
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-25 20:51:48 +0530

    pattern 8

--- patterns/pattern8.java
+++ patterns/pattern8.java
@@ -1,12 +1,30 @@
 public class pattern8 {
     public static void main(String[] args)
      {
+         int n = 5;
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n-i-1;j++){
+                System.out.print(" ");
+            }
+
+            for(int k =0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+
+            for(int l =0;l<n-i;l++){
+                System.out.print(" ");
+            }
+
+            System.out.println();
+            
+
+        }
        
       
         
-        for(int i=5;i>=0;i--){
+        for(int i=4;i>=0;i--){
         
-            for(int j =0;j<5-i;j++){
+            for(int j =0;j<4-i;j++){
                 System.out.print(" ");
             }
 
