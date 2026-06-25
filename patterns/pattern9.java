a8d404129f3b28267a43d38b28d50b24c25b98d2
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-25 21:05:21 +0530

    pattern 5

---
+++ patterns/pattern9.java
@@ -0,0 +1,21 @@
+public class pattern9 {
+    public static void main(String[] args) {
+      for(int i =0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println("");
+      } 
+
+      for(int k =0;k<4;k++){
+        for(int l =0;l<4-k;l++){
+  System.out.print("*");
+        }
+        System.out.println("");
+      }
+            
+        
+       
+    }
+    
+}
--- patterns/tempCodeRunnerFile.java
+++ patterns/tempCodeRunnerFile.java
@@ -1,4 +1,6 @@
-public class pattern7 {
-    public static void main(String[] args) {
-
-        for(int i =5;i>=0;i--){
+  for(int i =0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.println("");
+      } 
