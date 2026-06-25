64d35c45fd5b36aea01ca06b3b783a4ef587a34a
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-25 22:39:18 +0530

    pattern 12

---
+++ patterns/pattern12.java
@@ -0,0 +1,34 @@
+public class pattern12 {
+    public static void main(String[] args) {
+        for(int i =1;i<=4;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(j);
+            }
+             for(int j=1;j<=8-2*i;j++){
+    System.out.print(" ");
+            }
+
+            for(int k =1;k<=i;k++){
+        System.out.print(i-k+1);
+    }
+
+     System.out.println();
+          
+        }
+        // for(int i =1;i<=4;i++){
+    //         for(int j=1;j<=4-i;j++){
+    // System.out.print(" ");
+    //         }
+
+    // for(int k =1;k<=i;k++){
+    //     System.out.print(i-k+1);
+    // }
+
+   
+
+}      
+        }
+
+    
+    
+
--- patterns/tempCodeRunnerFile.java
+++ patterns/tempCodeRunnerFile.java
@@ -1,6 +1,1 @@
-  for(int i =0;i<5;i++){
-        for(int j=0;j<=i;j++){
-            System.out.print("*");
-        }
-        System.out.println("");
-      } 
+  System.out.print( i-k+1  );
