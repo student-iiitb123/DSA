b1fe1829f94219d3bf557201ad0aee1f4f5909b2
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-26 11:01:20 +0530

    pattern15

---
+++ patterns/pattern15.java
@@ -0,0 +1,12 @@
+public class pattern15 {
+    public static void main(String[] args) {
+            for(int i =0;i<=4;i++){
+                  for(char ch ='A';ch <='A'+(4-i);ch++){
+                    System.out.print(ch);
+                  }
+                  System.out.println();
+            }
+    
+    }
+    
+}
