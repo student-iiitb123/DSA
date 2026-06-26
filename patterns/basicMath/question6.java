a76a2b6058b420f41962c1f5b00fc92b4bd2ea1a
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-26 14:21:42 +0530

    question6

---
+++ patterns/basicMath/question6.java
@@ -0,0 +1,13 @@
+
+
+public class question6 {
+    public static void main(String[] args) {
+        int n =36;
+        for(int i =1;i<=n;i++){
+          if(n%i == 0){
+            System.out.println(i);
+          }
+        }
+    }
+    
+}
