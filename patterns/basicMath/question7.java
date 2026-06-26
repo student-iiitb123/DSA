a1c57b78f392100e3d5100eb57bc7617f4318fc8
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-26 14:28:45 +0530

    question7.java

---
+++ patterns/basicMath/question7.java
@@ -0,0 +1,15 @@
+public class question7 {
+    public static void main(String[] args) {
+        int n =10;
+        for(int i =2;i<=9;i++){
+            if(n%i == 0){
+                System.out.print(" not Prime number");
+                break;
+            }else{
+                System.out.println("palindrome");
+            }
+        }
+
+    }
+    
+}
