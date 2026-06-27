1303d604b62158ccf5190d984f01934826fdbf3d
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-27 15:51:26 +0530

    Selection Sort

---
+++ patterns/Hashing/Sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++){
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++){
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+}
+            }
+            int temp = arr[i];
+arr[i] = arr[minIndex];
+arr[minIndex] = temp;
+                
+        }
+
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+}
