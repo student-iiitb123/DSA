60ce1fc90384215973ce23b3201ffb760316f3b2
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-28 17:10:37 +0530

    Insertion Recurive approch

---
+++ patterns/Hashing/Sorting/Insersionsort.java
@@ -0,0 +1,18 @@
+public class Insersionsort {
+    public static void main(String[] args) {
+        int arr[] = {13,46,24,52,20,9};
+        for(int i =1;i<arr.length;i++){
+            int curr = arr[i];
+            int perv = i-1;
+            while(perv >= 0 && arr[perv]>curr){
+               arr[perv+1] = arr[perv];
+               perv--;
+            }
+            arr[perv+1] = curr;
+        }
+
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
