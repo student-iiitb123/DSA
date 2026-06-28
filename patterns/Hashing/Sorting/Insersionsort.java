4fc88632b6f99c7cb3f0ea876fee7ad61ac09c51
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-28 17:57:19 +0530

    Reccurive method to iterative

--- patterns/Hashing/Sorting/Insersionsort.java
+++ patterns/Hashing/Sorting/Insersionsort.java
@@ -1,18 +1,39 @@
 public class Insersionsort {
+   public static void inner(int arr[], int curr, int perv) {
+
+    if (perv < 0 || arr[perv] <= curr) {
+        arr[perv + 1] = curr;
+        return;
+    }
+
+    arr[perv + 1] = arr[perv];
+    inner(arr, curr, perv - 1);
+}
+    public static void outer(int arr[],int i){
+        if(i>=arr.length) return;
+        //work
+        inner(arr,arr[i],i-1);
+
+        outer(arr,i+1);
+    }
     public static void main(String[] args) {
         int arr[] = {13,46,24,52,20,9};
-        for(int i =1;i<arr.length;i++){
-            int curr = arr[i];
-            int perv = i-1;
-            while(perv >= 0 && arr[perv]>curr){
-               arr[perv+1] = arr[perv];
-               perv--;
-            }
-            arr[perv+1] = curr;
-        }
+        // for(int i =1;i<arr.length;i++){
+        //     int curr = arr[i];
+        //     int perv = i-1;
+        //     while(perv >= 0 && arr[perv]>curr){
+        //        arr[perv+1] = arr[perv];
+        //        perv--;
+        //     }
+        //     arr[perv+1] = curr;
+        // }
+
+        outer(arr,1);
 
         for(int i =0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
+
+
     }
 }
