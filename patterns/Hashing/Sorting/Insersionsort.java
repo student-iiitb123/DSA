eaf888a2bdb0038a42eb71d489e9a1b124537197
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-28 18:19:35 +0530

    iterative to Recursive4

--- patterns/Hashing/Sorting/Insersionsort.java
+++ patterns/Hashing/Sorting/Insersionsort.java
@@ -19,7 +19,7 @@ public class Insersionsort {
     public static void main(String[] args) {
         int arr[] = {13,46,24,52,20,9};
       
-        //     int curr = arr[i];
+     
         //     int perv = i-1;
         //     while(perv >= 0 && arr[perv]>curr){
         //        arr[perv+1] = arr[perv];
