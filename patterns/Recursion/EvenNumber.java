38e43093bc8c1bf876eb76318ec33a2acfaf4463
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-28 15:56:40 +0530

    Iterative to Recursive

---
+++ patterns/Recursion/EvenNumber.java
@@ -0,0 +1,27 @@
+public class EvenNumber{
+    public static void printEvenNumber(int arr[],int i){
+        if(i>=arr.length){
+            return;
+        }
+       if(arr[i] %2 ==0){
+        System.out.println(arr[i]);
+       }
+     printEvenNumber(arr, i+1);
+    } 
+        
+    
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        printEvenNumber(arr,0);
+
+        // for(int i =0;i<arr.length;i++){
+        //     if(arr[i] %2 ==0){
+        //         System.out.println(arr[i]);
+        //     }
+        // }
+
+        //step1 loop varibale become function parametre
+
+
+    }
+}
