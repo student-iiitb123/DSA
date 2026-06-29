aa97c301bd2963ab603dea69b13174502f17b53f
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-29 19:22:48 +0530

    Optimised

---
+++ patterns/ArrayEAsy/UseSet.java
@@ -0,0 +1,17 @@
+
+import java.util.HashSet;
+
+
+
+public class UseSet {
+    public static void main(String[] args) {
+        int arr[] = {5,2,4,2,1,5};
+       
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+        System.out.println(set);
+    }
+    
+}
