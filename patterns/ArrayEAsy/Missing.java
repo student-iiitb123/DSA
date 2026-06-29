50110bc1f3e93f11768ec272869a47eafb260f73
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-29 23:44:06 +0530

    missing Number

---
+++ patterns/ArrayEAsy/Missing.java
@@ -0,0 +1,25 @@
+
+
+public class Missing {
+   public static void main(String[] args) {
+        int n =8;
+    int sum =0;
+    int sum2 =0;
+    int arr[] ={8,2,4,5,3,7,1};
+    for(int i=1;i<=n;i++){
+        sum = sum +i;
+   }
+   int totalsum =sum;
+   for(int i =0;i<arr.length;i++){
+     sum2 = sum2 +arr[i];
+
+   }
+
+   System.out.println(totalsum - sum2);
+
+
+
+    }
+
+    
+}
