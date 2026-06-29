3a8421492c708496f61da75d3209344b9f836d43
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-29 23:27:06 +0530

    moves zeroes

---
+++ patterns/ArrayEAsy/MoveZero.java
@@ -0,0 +1,41 @@
+
+
+
+
+public class MoveZero {
+    public static void main(String[] args) {
+        int arr[] = {1,0,2,3,0,4,0,1};
+      int i =0;
+    int j =0;
+    while(j<arr.length && i<arr.length){
+        if(arr[i] != 0){
+            i++;
+            j++;
+
+        }
+        else{
+            j++;
+           if(j<arr.length && arr[j] != 0){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+           }else{
+        while(j < arr.length && arr[j] != 0){
+                j++;
+             int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            }
+           }
+        }
+
+    }
+
+    for(int l =0;l<arr.length;l++){
+        System.out.print(arr[l]);
+    }
+    }
+    
+}
