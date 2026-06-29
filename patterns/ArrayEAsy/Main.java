7df0975e02dfbb9a8b043adc2265e365ef984d9e
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-29 19:37:43 +0530

    main Quetion

---
+++ patterns/ArrayEAsy/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static int removeDuplicates(int arr[]) {
+
+        int i = 0;
+
+        for (int j = 1; j < arr.length; j++) {
+
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,1,2,2,3,4,4,5};
+
+        int size = removeDuplicates(arr);
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
