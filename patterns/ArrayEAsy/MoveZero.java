71ee0c917ffa07e54d0761489bbc75d0944a2449
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-06-30 10:21:48 +0530

    Move Zero to end

--- patterns/ArrayEAsy/MoveZero.java
+++ patterns/ArrayEAsy/MoveZero.java
@@ -1,41 +1,30 @@
-
-
-
-
 public class MoveZero {
     public static void main(String[] args) {
         int arr[] = {1,0,2,3,0,4,0,1};
-      int i =0;
-    int j =0;
-    while(j<arr.length && i<arr.length){
-        if(arr[i] != 0){
+        //find first zero;
+        int i =0;
+        while(i<arr.length && arr[i] != 0){
             i++;
-            j++;
-
         }
-        else{
-            j++;
-           if(j<arr.length && arr[j] != 0){
+        int j = i+1;
+        while(j<arr.length ){
+            if(arr[j] != 0){
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
-            i++;
-           }else{
-        while(j < arr.length && arr[j] != 0){
-                j++;
-             int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+
             i++;
             }
-           }
+             j++;
+
         }
+         for(int k =0;k<arr.length;k++){
+            System.out.print(arr[k]);
+         }
+       
+     
+    }
+    
 
-    }
-
-    for(int l =0;l<arr.length;l++){
-        System.out.println(arr[l]);
-    }
-    }
     
 }
