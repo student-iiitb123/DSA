cd6ec07dcfcb7ebbd4af0bf351d6cc817cd011a8
Ashish singh Goutam <ashishgautam7696@gmail.com>
2026-07-03 10:32:10 +0530

    prefix sum

--- patterns/ArrayEAsy/PrefixSum.java
+++ patterns/ArrayEAsy/PrefixSum.java
@@ -2,6 +2,15 @@ public class PrefixSum {
     public static void main(String[] args) {
         int arr[] = {9,4,20,3,10,5};
         int k =33;
+        int ps[] = new int[arr.length];
+        int sum = 0;
+        for(int i =0;i<arr.length;i++){
+          sum = sum +arr[i];
+          ps[i] =sum;
+        }
+        for(int i =0;i<ps.length;i++){
+         System.out.println(ps[i]);
+        }
     }
 
 }
